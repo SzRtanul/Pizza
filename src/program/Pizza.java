46011ed0bf9ab267dd71e7521daf20b2b63aedf6
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2023-10-17 11:12:57 +0200

    1

---
+++ src/program/Pizza.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package program;
+
+/**
+ *
+ * @author SzabóRoland(SZOFT_20
+ */
+public class Pizza {
+    String vastagsag;
+    String alap;
+    int atmero;
+    String[] osszetevok;
+    
+    public Pizza(){
+    
+    }
+}
---
+++ src/program/Rendeles.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package program;
+
+import java.io.*;
+
+/**
+ *
+ * @author SzabóRoland(SZOFT_20
+ */
+public class Rendeles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        String[][] pizza = new String[][]{
+            {"Vastagság: ", "Vastag", "Vékony"},
+            {"Alap: ", "Paradicsomos", "Tejfölös"},
+            {"Átmérő: ", "32cm", "45cm"},
+            {"Összetevők: ", "Édes", "Hús", "Tejtermék és egyebek"},
+            {"Átsültség mértéke: ", "Nyers", "Félig nyers", "Sült", "Túl sült", "Égett", "Hamu"}
+        };
+        String[][] osszetevok = new String[][]{
+            {"Édes: ", "Ananász", "Nutella", "Vanilliakrém", "Tejszínhab", "Csokiöntet", "Lekvár"},
+            {"Hús: ", "Bacon", "Sonka", "Kolbász", "Hurka", "Sós karamella krém"},
+            {"Tejtermék és egyebek: ", "Tojás", "Dupla sajt"}
+        };
+        FileWritn(pizza, "pizza.txt");
+        FileWritn(osszetevok, "osszetevok.txt");
+        
+        
+        
+    }
+    
+    public static void FileWritn(String[][] arrayi, String filename){
+        File fil = new File(filename);
+        try (PrintWriter writer = new PrintWriter(fil, "UTF-8")) {    
+            for (int i = 0; i < arrayi.length; i++) {
+                String szoveg = arrayi[i][0]+",";
+                for (int j = 0; j < arrayi[i].length; j++) {
+                    szoveg+=arrayi[i][j]+",";
+                }
+                writer.println(szoveg);
+            }
+        } catch (FileNotFoundException ex) {
+            //Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            //Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void FileReadn(String[][] arrayi, String filename){
+        
+        try (InputStreamReader reader = new InputStreamReader(fil, "UTF-8")) {    
+            reader.
+            while(reader.read() >= 0){
+                
+            }
+        } catch (FileNotFoundException ex) {
+            //Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            //Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
